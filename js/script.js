4a7d30ba717d7adde2a70ad9f040b6cc0de33cb1
Gabriel Regus <gabriel.regus@gmail.com>
2023-05-15 18:57:30 -0400

    adding the Query API and Extractor APIs

--- js/script.js
+++ js/script.js
@@ -1,3 +1,56 @@
+//This will JS for company profile
+const requestURL =
+  "https://api.sec-api.io?token=c1484b557f33e9b69965330859cc4392e4c419f138b4cdf500e4d7155e8bd39b";
+const apiKey =
+  "c1484b557f33e9b69965330859cc4392e4c419f138b4cdf500e4d7155e8bd39b";
+
+const element = document.querySelector("#post-request .company_name");
+const requestOptions = {
+  method: "POST",
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    query: {
+      query_string: {
+        query:
+          'ticker:AAPL AND formType:"10-K" AND documentFormatFiles.type:"EX-21"',
+      },
+    //   from: "0",
+    //   size: "10",
+    //   sort: [
+    //     {
+    //       filedAt: ['2022-01-01 TO 2022-12-31'],
+    //     },
+    //   ],
+    },
+  }),
+};
+
+fetch(requestURL, requestOptions)
+  .then((response) => response.json())
+  .then(function (data) {
+    console.log(data.filings[0].linkToHtml);
+    var extractorUrl = data.filings[0].linkToHtml
+    extractorFetch(extractorUrl)
+  });
+
+  function extractorFetch(url){
+    // https://api.sec-api.io/extractor?
+    // url=url
+    // item=1A&
+    // type=text&
+    // token=YOUR_API_KEY
+
+
+    fetch('https://api.sec-api.io/extractor?url='+url+'item=1A&type=text&token='+ apiKey)
+    .then(function(response){
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data);
+    })
+  }
+
+
 //  POST request with a JSON body using fetch
 var requestUrl = 'https://api.sec-api.io?token=c1484b557f33e9b69965330859cc4392e4c419f138b4cdf500e4d7155e8bd39b'
 const element = document.querySelector('#post-request .company-');
@@ -16,22 +69,3 @@ fetch(requestUrl, requestOptions)
     console.log(data.filings[0].documentFormatFiles[0].documentUrl);
    }
 });
-
-
-// linkToHtml
-
-    //Start with the Full-Text Search API 
-    // https://sec-api.io/docs/full-text-search-api
-// Search by company name, keyword, ticker, CIK number, and/or reporter's last name—individually or in combination.
-//we will need the filingUrl (string) - URL of the filing or attachement. Example: https://www.sec.gov/Archives/edgar/data/65011/000006501121000020/fy21q2exh99earnings.htm
-// Boolean operators allow for searches where all words entered are required to be in the resulting documents, exact phrases matching, exclude terms, OR logic clauses, and wildcards. Any of these can be combined in a single search.
-
-
-//Next move onto Extractor API
-// https://sec-api.io/docs/sec-filings-item-extraction-api
-// Once we have the filingUrl (string) - URL of the filing or attachement. Example: https://www.sec.gov/Archives/edgar/data/65011/000006501121000020/fy21q2exh99earnings.htm
-// we could use that filingUrl (string) to pull the desired company data from the foll
-
-
-
-
